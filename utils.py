ffce3cdcb0e546e69a5825fd0290f91efae5671b
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-25 11:45:25 +0200

    changed the keys storing system

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 # Imports
+import json
 
 # General imports
 
 # LLM imports
 
 # Local imports
+
+def get_local_keys():
+    """Get local keys from a local keys.json file."""
+    with open("keys.json", "r") as f:
+        keys = json.load(f)
+    return keys
