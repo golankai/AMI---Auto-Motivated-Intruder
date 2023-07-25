291c7b22cc94a7e7b2faed5d988aa3054b940969
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-25 15:22:07 +0200

    created a anonimizer class

--- utils.py
+++ utils.py
@@ -1,14 +1,16 @@
-# Imports
 import json
 
-# General imports
 
-# LLM imports
 
-# Local imports
 
 def get_local_keys():
     """Get local keys from a local keys.json file."""
     with open("keys.json", "r") as f:
         keys = json.load(f)
     return keys
+
+def get_prompts_templates():
+    """Get prompts template from a local prompts.json file."""
+    with open("prompts.json", "r") as f:
+        prompts = json.load(f)
+    return prompts
