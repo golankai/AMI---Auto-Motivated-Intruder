2b1a44770eefac637f95e31652916cccef16074b
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-25 11:45:25 +0200

    changed the keys storing system

--- de_anonimiser.py
+++ de_anonimiser.py
@@ -3,7 +3,7 @@
 # General imports
 
 # LLM imports
-from langchain.llms import OpenAI, ChatOpenAI
+from langchain.llms import OpenAI, HuggingFaceHub, Cohere
 from langchain.prompts.chat import (
     PromptTemplate,
     ChatPromptTemplate,
@@ -13,17 +13,30 @@ from langchain.prompts.chat import (
 from langchain.chains import LLMChain
 from langchain.schema import BaseOutputParser
 
+from llamaapi import LlamaAPI
+from langchain_experimental.llms import ChatLlamaAPI
+
 # Local imports
+from utils import get_local_keys
+
+# Get local keys
+keys = get_local_keys()
+
+# Define prompts
+prompt = PromptTemplate.from_template("Please re-identify the following person: {product}")
 
 # Define the LLM
-llm = OpenAI(openai_api_key="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")
+llm = OpenAI(openai_api_key=keys["openai_api_key"])
+llama = LlamaAPI(keys["llama_api_key"])
+llama_model = ChatLlamaAPI(client=llama)
 
 
-prompt = PromptTemplate.from_template("Please re-identify the following person: {product}")
-print(prompt.format(product="ANONIMISEDTEXT"))
+# Define the chain
+
+# Run the chain
+print(llama_model.predict("Hello, how are you?"))
 
 
 
 
 
-print(prompt)
