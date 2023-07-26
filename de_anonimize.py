d2b259f580a3b3426af439e1139ec7a89fc07549
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-26 17:28:38 +0200

    added a read_data func

--- de_anonimize.py
+++ de_anonimize.py
@@ -1,14 +1,18 @@
+import pandas as pd
+
+from utils import read_data
 from DeAnonimiser import DeAnonimiser
 
 # Path to anonimised text
 anon_text_path = "textwash_data/study2/person_descriptions/anon/adele_57.txt"
 
-# Set up the de-anonimiser
-de_anonimiser = DeAnonimiser()
-
-# Define the text to de-anonimise
+# Read the data
 with open(anon_text_path, "r") as f:
     anon_text = f.read()
+# df = read_data("textwash_data/study2/person_descriptions/anon")
+
+# Set up the de-anonimiser
+de_anonimiser = DeAnonimiser(llm="llama2")
 
 # Run the de-anonimiser
 result = de_anonimiser.de_anonymise(anon_text)
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import json
-
-
-
+import os
+import pandas as pd
 
 def get_local_keys():
     """Get local keys from a local keys.json file."""
@@ -14,3 +13,20 @@ def get_prompts_templates():
     with open("prompts.json", "r") as f:
         prompts = json.load(f)
     return prompts
+
+def read_data(dir: str) -> pd.DataFrame:
+    """
+    Read data from a directory to a panda DataFrame.
+    """
+
+    files = os.listdir(dir)
+    df = pd.DataFrame(columns=["file_name", "anon_text"])
+    anon_texts = []
+    for file in files:
+        with open(os.path.join(dir, file), "r", encoding='utf-8') as f:
+            anon_texts.append(f.read())
+
+    df["file_name"] = files
+    df["anon_text"] = anon_texts
+    return df
+    
