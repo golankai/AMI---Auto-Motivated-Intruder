ae7110336029d0464290c43b332c21597ad2ad1c
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-26 18:10:23 +0200

    small change

--- de_anonimize.py
+++ de_anonimize.py
@@ -4,20 +4,21 @@ from utils import read_data
 from DeAnonimiser import DeAnonimiser
 
 # Path to anonimised text
-# anon_text_path = "textwash_data/study2/person_descriptions/anon/adele_57.txt"
-anon_texts_path = "textwash_data/study2/person_descriptions/anon"
+anon_text_path = "textwash_data/study2/person_descriptions/anon/adele_57.txt"
+# anon_texts_path = "textwash_data/study2/person_descriptions/anon"
 
 # Read the data
-# with open(anon_text_path, "r") as f:
-#     anon_text = f.read()
-df = read_data("textwash_data/study2/person_descriptions/anon")
-df = df.head(10)
+with open(anon_text_path, "r") as f:
+    anon_text = f.read()
+# df = read_data("textwash_data/study2/person_descriptions/anon")
+# df = df.head(10)
 
 # Set up the de-anonimiser
 de_anonimiser = DeAnonimiser(llm="flan-t5")
 
 # Run the de-anonimiser
-# result = de_anonimiser.de_anonymise(anon_text)
-df['de_anon_result'] = df.apply(lambda row: de_anonimiser.de_anonymise(row["anon_text"]), axis=1)
+result = de_anonimiser.de_anonymise(anon_text)
+# df['de_anon_result'] = df.apply(lambda row: de_anonimiser.de_anonymise(row["anon_text"]), axis=1)
+
 # Print the result
-print(df)
+print(result)
