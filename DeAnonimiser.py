2b733950ffe85e08d029edd045b5e03afbd5f65d
Kai Golan Hashiloni <golankai@gmail.com>
2023-07-26 17:28:38 +0200

    added a read_data func

--- DeAnonimiser.py
+++ DeAnonimiser.py
@@ -16,34 +16,54 @@ from langchain_experimental.llms import ChatLlamaAPI
 
 from utils import get_local_keys, get_prompts_templates
 
+# helper functions
+
+def get_repo_id(llm: str):
+    match llm:
+        case "flan-t5":
+            repo_id = "google/flan-t5-xxl"
+        case "llama2":
+            repo_id = "meta-llama/Llama-2-70b-hf"
+        case _:
+            # raise an exception
+            raise ValueError("llm name is not valid") 
+    return repo_id
+
 class DeAnonimiser():
-    def __init__(self):
+    '''
+    Class of a de-anonimiser.
+    '''
+    def __init__(self, llm: str):
+        '''
+        Create a new instance of a de-anonimiser.
+        :param llm: The LLM to use. Must be one of ['flan-t5' or 'llama2'].
+        '''
 
         # Accesses and keys
         keys = get_local_keys()
         os.environ["HUGGINGFACEHUB_API_TOKEN"] = keys["huggingface_hub_token"]
 
+        # Define the LLM
+        repo_id = get_repo_id(llm)        
+        self.llm = llm = HuggingFaceHub(
+            repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 512}
+        )
+        
+
     def de_anonymise(self, anon_text):
         # Define prompts
         templates = get_prompts_templates()
         prompt = PromptTemplate(template=templates["flanT5"]["pls_de_identify"], input_variables=["anon_text"])
-
-        # Define the LLM
-        # repo_id = "meta-llama/Llama-2-70b-hf"
-        # repo_id = "meta-llama/Llama-2-70b-hf"
-
-        repo_id = "google/flan-t5-xxl"
-
-        llm = HuggingFaceHub(
-            repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 512}
-        )
+        
 
         # Define the chain
-        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        llm_chain = LLMChain(prompt=prompt, llm=self.llm)
 
         # Run the chain
         result = llm_chain.run(anon_text)
         return result
+    
+
 
 
 # llm = OpenAI(openai_api_key=keys["openai_api_key"])
