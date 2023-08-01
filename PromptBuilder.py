adfde457e12c748328646f8a8910f6f2943286aa
Kai Golan Hashiloni <golankai@gmail.com>
2023-08-01 11:48:20 +0200

    still working on the output parser

--- PromptBuilder.py
+++ PromptBuilder.py
@@ -4,6 +4,8 @@ from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field, validator
 
 
 class PromptBuilder:
@@ -29,8 +31,9 @@ class PromptBuilder:
                 "characteristics_cands": "Please provide a list of 5 critical characteristics about the person.\nRule: the characteristics should include NO placeholders such as PERSON_FIRSTNAME_1, LOCATION_1, OCCUPATION_1, etc. For each characteristic, please provide me a list of 5 persons that could match it\nAnswer in the following format exactly:\n1. CHAR1\n- PERSON1\n...\n2. CHAR2\n...",
             },
             "llama2": {
-                "pls_de_identify": """Task: please re-identify the person in the following anonymized text. First answer if you can, then the name or FAIL. Also give a score re-identifiability: 0 is very easy to re-identidy and 1 is impossible.
+                "pls_de_identify": """Task: please re-identify the person in the following anonymized text.
                 {format_instructions}
+                The Anonimized text is:,
                 {anon_text}""",
                 "fail2re-identify": "please provide critical characteristics about the person and 5 specific Google searches you'd submit to re-identify this person.\nRules: Both the characteristics and the Google search prompts should include no placeholders such as PERSON_FIRSTNAME_1, LOCATION_1, OCCUPATION_1, etc, as we don't know them.",
                 "re-identified": "Please supply the information that helped you re-identify the person.",
@@ -64,29 +67,35 @@ class PromptBuilder:
         Get a prompt template.
         """
         match prompt_name:
+            case "base":
+                return self._get_base_prompt()
             case "pls_de_identify":
                 return self._get_pls_de_identify_prompt()
-            case "llama2":
-                pass
             case _:
                 # raise an exception
                 raise ValueError("prompt name is not valid")
             
+    def _get_base_prompt(self) -> tuple[PromptTemplate]:
+        prompt = PromptTemplate(
+            template=self.get_template("base"),
+            input_variables=["history", "input"],
+        )
+        return prompt
+
 
     def _get_pls_de_identify_prompt(self) -> tuple[PromptTemplate, StructuredOutputParser]:
-        response_schemas = [
-        ResponseSchema(name="re-identifiable", description="Is it re-identifiable?", type="boolean"),
-        ResponseSchema(name="name", description="Name of the person or FAIL", type="str"),
-        ResponseSchema(name="score", description="The score or re-identifiabilization", type="float")
+        class FirstTry(BaseModel):
+            # re_identifiable: bool = Field(description="Is it re-identifiable?")
+            name: str = Field(description="Name of the person or FAIL")
+            # score: float = Field(description="The score or re-identifiabilization, 0 is very easy to re-identidy and 1 is impossible")
 
-        ]
-        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-        format_instructions = output_parser.get_format_instructions()
+        # Set up a parser + inject instructions into the prompt template.
+        parser = PydanticOutputParser(pydantic_object=FirstTry)
 
         prompt = PromptTemplate(
             template=self.get_template("pls_de_identify"),
             input_variables=["anon_text"],
-            partial_variables={"format_instructions": format_instructions},
-            output_parser=output_parser,
+            partial_variables={"format_instructions": parser.get_format_instructions()},
+            output_parser=parser,
         )
-        return prompt, output_parser
+        return prompt, parser
