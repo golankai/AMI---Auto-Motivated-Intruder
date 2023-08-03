7f627f206a0852838d140370cc61184e88c0394e
Kai Golan Hashiloni <golankai@gmail.com>
2023-08-03 17:28:00 +0200

    tried with google search agent

--- utils.py
+++ utils.py
@@ -63,6 +63,8 @@ def load_google_search_tool():
     Load the google search tool.
     """
     keys = get_local_keys()
+    os.environ["GOOGLE_API_KEY"] = keys["google_api_key"]
+    os.environ["GOOGLE_CSE_ID"] = keys["google_cse_id"]
     search = load_tools(["google-search"])[0]
     search.description = "A wrapper around Google Search. Useful for when you need to answer questions about current events or look for people who answer a specific charachteristic. Input should be a search query."
     return search
